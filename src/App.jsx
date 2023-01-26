f6d300e76e89634a55febbb861f5696c254997af
Franco Rodrigo Zabala Nogales <fzabala@khronoscloud.com>
2023-01-25 19:21:22 -0500

    ternarios

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,30 @@ const MyButton = ()=>{
     <button>Soy un boton</button>
   )
 }
+
+const Tenario1=()=>{
+  return
+  <>
+    online
+  </>
+}
+const Tenario2=()=>{
+  return
+  <>
+    <h1>offline</h1>
+  </>
+}
 const App = () => {
-  const title="Mi titulo desde una constante"
-  const classTitle = "text-center"
-  const pathImg="https://picsum.photos/200/300"
+  const title="Mi titulo desde una constante";
+  const classTitle = "text-center";
+  const pathImg="https://picsum.photos/200/300";
+  const user=true;
   return (
     <>
       <h1 className={classTitle}> {title.toLocaleUpperCase()}</h1>
       <img src={pathImg} alt={`imagen-${title}`}/>
       <MyButton/>
+      {user ? Tenario1:Tenario2}
     </>
   )
 };
